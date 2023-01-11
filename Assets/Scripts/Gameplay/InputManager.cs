065c3e2d316b4fffc5e6bc5428a6a5a9a7d22190
Ben Delaney-Brownlow <bendb923@gmail.com>
2023-01-11 11:59:30 +0100

    updated controls UI

--- Assets/Scripts/Gameplay/InputManager.cs
+++ Assets/Scripts/Gameplay/InputManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using Tetrified.Scripts.Gameplay;
 using Tetrified.Scripts.Utility;
@@ -64,24 +65,55 @@ namespace Tetromino.Scripts.Gameplay
 
         private void HandleInput(Vector2 inputPoint)
         {
-            bool leftSide = (inputPoint.x / Screen.width) < 0.5f;
-            bool bottomSide = (inputPoint.y / Screen.height) < 0.5f;
+            float xClickPercent = (inputPoint.x / Screen.width);
+            float yClickPercent = (inputPoint.y / Screen.height);
 
-            if (leftSide && bottomSide && bottomSide)
+            int xThird = Mathf.FloorToInt((int)(xClickPercent * 9) / 3);
+            int yThird = Mathf.FloorToInt((int)(yClickPercent * 9) / 3);
+
+            Debug.Log("xThird: " + xThird + ", yThird: " + yThird);
+
+            switch (xThird)
             {
-                GameManager.Instance.HandleInputActions(Action.RotateCounterClockwise);
-            }
-            else if (leftSide == false && bottomSide)
-            {
-                GameManager.Instance.HandleInputActions(Action.RotateClockwise);
-            }
-            else if (leftSide && bottomSide == false)
-            {
-                GameManager.Instance.HandleInputActions(Action.SelectTetrisBoardLeft);
-            }
-            else if (leftSide == false && bottomSide == false)
-            {
-                GameManager.Instance.HandleInputActions(Action.SelectTetrisBoardRight);
+                case 0:
+                    switch (yThird)
+                    {
+                        //bottom left third
+                        case 0:
+                            GameManager.Instance.HandleInputActions(Action.MoveTetronimoLeft);
+                            break;
+
+                        //middle left third
+                        case 1:
+                            GameManager.Instance.HandleInputActions(Action.RotateCounterClockwise);
+                            break;
+
+                        //top left third
+                        case 2:
+                            GameManager.Instance.HandleInputActions(Action.SelectTetrisBoardLeft);
+                            break;
+                    }
+                    break;
+                case 2:
+                    switch (yThird)
+                    {
+                        //bottom right third
+                        case 0:
+                            GameManager.Instance.HandleInputActions(Action.MoveTetronimoRight);
+                            break;
+
+                        //middle right third
+                        case 1:
+                            GameManager.Instance.HandleInputActions(Action.RotateClockwise);
+                            break;
+
+
+                        //top right third
+                        case 2:
+                            GameManager.Instance.HandleInputActions(Action.SelectTetrisBoardRight);
+                            break;
+                    }
+                    break;
             }
         }
     }
